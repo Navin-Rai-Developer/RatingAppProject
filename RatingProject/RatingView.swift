31df1fc1c3419fb06ff10b093883831604387988
Navin Rai <rai.navinrai20@gmail.com>
2026-01-26 15:43:31 +0530

    Add rating view component

--- RatingProject/RatingView.swift
+++ RatingProject/RatingView.swift
@@ -8,11 +8,53 @@
 import SwiftUI
 
 struct RatingView: View {
+    let maxRating: Int
+    @Binding var currentRating: Int
+    let width: Int
+    
+    init(maxRating: Int, currentRating: Binding <Int>, width: Int = 30) {
+        self.maxRating = maxRating
+        self._currentRating = currentRating
+        self.width = width
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(systemName: "star")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.yellow)
+                .symbolVariant(.slash)
+                .opacity(currentRating == 0 ? 0 : 1)
+                .onTapGesture {
+                    withAnimation {
+                        currentRating = 0
+                    }
+                }
+            ForEach(0..<maxRating, id: \.self) {
+                rating in
+                Image(systemName: "star")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundColor(.yellow)
+                    .symbolVariant(rating < currentRating ? .fill : .none)
+                    .onTapGesture {
+                        withAnimation {
+                            currentRating = rating + 1
+                        }
+                    }
+            }
+        }
+        .frame(width: CGFloat(maxRating * width))
     }
 }
 
 #Preview {
-    RatingView()
+    struct PreviewWrapper: View {
+        let maxRating = 5
+        @State var currentRating = 2
+        var body: some View {
+            RatingView(maxRating: maxRating, currentRating: $currentRating)
+        }
+    }
+    return PreviewWrapper()
 }
