6fac9a993241d142fb06f39ae0a79675ca4c0d90
Navin Rai <rai.navinrai20@gmail.com>
2026-01-26 14:55:03 +0530

    Rating Project some changes

---
+++ RatingProject/RatingView.swift
@@ -0,0 +1,18 @@
+//
+//  RatingView.swift
+//  RatingProject
+//
+//  Created by Navin Rai on 26/01/26.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RatingView()
+}
