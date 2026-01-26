6ca6eb635a09f50a30c8f45de4a5bea51ae45837
Navin Rai <rai.navinrai20@gmail.com>
2026-01-26 14:06:01 +0530

    Initial Commit

---
+++ RatingProject/RatingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatingProjectApp.swift
+//  RatingProject
+//
+//  Created by Navin Rai on 26/01/26.
+//
+
+import SwiftUI
+
+@main
+struct RatingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
